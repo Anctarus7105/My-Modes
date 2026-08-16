a2f01675433aed0d34d944ff55119e2d1eca6cd6
Anctarus7105 <idzhabkin@gmail.com>
2026-08-16 20:03:28 +0400

    Implement Ghostly mode with multiple entities
    Add Ghostly mode with various features and spawns.

---
+++ Ghostly.lua
@@ -0,0 +1,139 @@
+require(game.Players.LocalPlayer.PlayerGui.MainUI.Initiator.Main_Game).caption("t̷͎̝̫̟̖̝ͦ͆̀͊̑ͬ͑ḩ̶̡̤̤͎̦͇̤͙͔͙̞̼̣̼͍̱̗̍̃͗̔ͪ̌̌͊ͬ̐̿ͪ̋͋͜͠_̭̻͕́̽́ỉ̤͎̅ṣͭͤ͠_̣͕ͩ̒́̀̾̄͞_̷̻͚̀͋ͅ_̯͈̝́̃̋ m̴̵̶̧̦̹̱̮͚͍̘̉̇ͮ̏ͯͩ̀̍̿͘͘̕͘̚͝͞o̶ͭ_̡͓̙͓̱ͪͦ͊̈́̅͛͘͟ḓ̵̡̡̨̭̘̼͈̹̭̈̊͗͗̋͐̽͟ȩ̦̺̟̞̰̳̭̿̆ͭ̐̾́́̍̃ͬ̐͘͠ w̜͕͑a̵̫̠̝̜̤̱̫̥̘ͫ̃͊̆ͪ̌ͣ͗͐̌ͥ͗ͭ̚͝͞ͅs̛̘͉̗̾͡_̴̷̢͍̲̗̹̰̺ͣ͆͂̆̀̅̓̂͞ m̢͈̤̒̉̍̒̓͠a̶̢̬͍͔̟͚͈̪͍͉̩̗̯̝̳ͥ̿̋̃͊͆ͬͥ̀͆̄͂͘͢͜͡d̵̵̮̲̹̫̪̬̦̉ͣ̓̽ͩͭͤ̊̍̽̒̒̓̓̐͝͠ḛͪ̐ b̵̴̵̨̟̤̗͓͔̪̬̟̺͍̖̗̲͎̼̳̗̓ͮ́ͪͬͪ̓̑ͯ̿ͣͧ̐̎ͪ̇͛̓ͭͣ̀͠yͪ Ă̧̢̗̈ͤ̾_̛_̸̧̠̝̙̫̞͍͓̜̲̬̗̳̑̈́ͦ̈́͑͑ͧ͐̿͑̽̑͘͡ͅn̴̵̮̦̻̥͐͂͝c̶̶̴̭̪̳̳̫͚̬͈͉͉̈͑ͦ͊̑͑ͮ̎̐̓͆ͩ̋ͤ͟͜͝͞͡t͆ͯà̡̳͍͙̫̻̲͖͕̟̺̩̔̉͛ͬ̊ͣ͘͘̕͟ř̵̼̹͂̏ͩ̉̅̇͢u̷̸̧̗̦͙ͣͥ̈́̓̓ͮ̀͆̇̋̑͡ͅ_ͬͅs̵̸̮̗̖̗̭͖̼̦̰͇ͧ͋ͯ̽͒́͗͊̚͟͠#̨͈̪̯̹̲̹̦̫͎͕̋̐ͤ̓̆͐̿̔̔̐͠͞7͕̝ͣͯ̏1̹̖̞̲̟́̓ͤͫ͟͞0̶̶̜͚̘̤͈̟͕́̈́ͤ̾̊̽ͩͥͧ̏̂̉̎̉̽͆̀̔͌̈̂͂̇̕̕̕͘͘͢͠͠5̶̶̸̵̪͍̯̤̅́̀̇͌́͝͝",true)
+game.ReplicatedStorage.GameData.LatestRoom.Changed:Wait()
+wait(1)
+require(game.Players.LocalPlayer.PlayerGui.MainUI.Initiator.Main_Game).caption("L̵͈̩̞̲̜̗ͤ́͋_̴̜͖̱͔̬̺̭̐ͫ̎̌ͨ̄ͩ͌̒̍͂̅͂͢o̸̴͚̜͕̼̳̰͔̪ͯͧͤ̀̋͛̇ͩ̂ͤ̍ͯ̓̀̿̈ͣ̀ͭͦ͜͞a̷̷̳͚̣ͨ̓ͤ̏̓̆̆̚͞d̶̸̢̛̳̝͉͍̯̲̣̺̠̟̮̣͍͇̭̿ͥ̊̂́͋ͬ͆ͥͪͭ̊̈̿̀ͣͧ͂̄̏͟͟͜͝e̷̗̍̀͆̈́͟d̢̛̦̫͎̯̜̖͙̞̰͆̓̆͌͛̌̿͋̽ͮ͛ͮ̆̾̕͘͟͡!",true)
+wait(3)
+require(game.Players.LocalPlayer.PlayerGui.MainUI.Initiator.Main_Game).caption("Gͤ̚h̨͕̰͑̓̋͞_͖̬̙̜̒̓́ͫͦ͗̚ơ̥̻͇͈̣̩͆͆̐͒̎̾͑́̍ͭ͠͝ͅs̜͎̘͔̩͖̱̤̠̱̀ͦ̀̓ͥ̃̓̏̏̍̓̈ͨͬ͟t̺̥̪͉̺͎̼̫͛ͣ͗̀ͪ̏̓̀͆ͥ_̷̸̻͚͓̱̗͐ͧ̔͛͛̈́̓̃́̿̚sͥͪ͂͟͡ a̵̡̛̛̱͇͈̩̤̪̙͒ͯ̓͌̋̉͋̐̊͗͟͝͞r̡̩̫̻̟̩̩͖̻̤̠̯̻̝̞̲̥̗͂̇̿ͪ̏ͧ͗̃̅ͦ̈̓͋̎̊ͥ̾̕ͅe̸̡̤̫͈͈̘̪̖ͪ̀ͤ̂̉͆ͬ͐́ͩ͛ͩ͐͡ h̴̜̼̻͓͔̪̥̦̭̎̏́ͭ͡ȩ̨̨̢̦̠̯̺̞͍͚̪͉̣̥ͭ̉̃̂̎̾ͯ͊̐͌ͦ́̿̅̉ͪ̆̚͘r̡̛͖̯̮̞͕̤̮̔̋͗̏̾͊ͨͭ̾̆̕ͅe̡̪̯̩̱̖̰͍̦̲̰̹ͣ̔͂̌ͧ̅ͩ̏̿̆͢͝͡͞ no̱̱̾̃͒͆_̴̢̞̞̠͕̗̊͊ͤͩ̆̓͆̑͂ͦͥ͛̉̕͞_̩̍̒_̷̨̞͕̰̳̯̝̏́͋w̵̯̤̰̫̮͚̎̍͐̐̊̕͜.̨̢̛̻͍̜̈́ͮ̅̒̄̍ͮ̾͟͝.͚̃ͧ̀.̶̢̧͈̣̞̫̟̖͍̜̫̟͇̦̲͔͙̓ͯ́̀̄̀ͭ̄ͤ̽̀͐ͤ̃ͭ̀ͦͥ̕͝͠ͅ",true)
+
+-- Sprint Bar
+loadstring(game:HttpGet("https://raw.githubusercontent.com/johnnkcool/Doors-hardmode/refs/heads/main/doors-hardcore-req.lua"))()
+
+-- Ambient
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Anctarus7105/Random-Files/refs/heads/main/ambient.lua"))()
+
+-- Fog
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Anctarus7105/Random-Files/refs/heads/main/Fog%20For%20Doors"))()
+
+-- Black Eyes
+coroutine.wrap(function()
+    while true do
+        wait(math.random(30, 50))
+        game.ReplicatedStorage.GameData.LatestRoom.Changed:Wait()
+        wait(3)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/eoyoustme/Extremely/refs/heads/main/Black%20Eyes"))()
+    end
+end)()
+
+-- Ripper
+coroutine.wrap(function()
+    while true do
+        wait(115)
+        game.ReplicatedStorage.GameData.LatestRoom.Changed:Wait()
+        wait(0)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/eoyoustme/Extremely/refs/heads/main/Ripper%20Spawner"))()
+    end
+end)()
+
+-- A-60 prime
+coroutine.wrap(function()
+    while true do
+        wait(math.random(20, 90))
+        game.ReplicatedStorage.GameData.LatestRoom.Changed:Wait()
+        wait(6)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/eoyoustme/Monoxide/refs/heads/main/A-60%20Prime"))()
+    end
+end)()
+
+-- A-666
+coroutine.wrap(function()
+    while true do
+        wait(math.random(80, 160))
+        game.ReplicatedStorage.GameData.LatestRoom.Changed:Wait()
+        wait(4)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/eoyoustme/Monoxide/refs/heads/main/A-666"))()
+    end
+end)()
+
+-- shocker
+coroutine.wrap(function()
+    while true do
+        wait(35)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/eoyoustme/Script/refs/heads/main/Daze.txt"))()
+  end
+end)()
+
+-- Pandemonium
+coroutine.wrap(function()
+    while true do
+        wait(math.random(100, 250))
+        game.ReplicatedStorage.GameData.LatestRoom.Changed:Wait()
+        wait(5)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/eoyoustme/Monoxide/refs/heads/main/Pandemonium"))()
+    end
+end)()
+
+-- G-95
+coroutine.wrap(function()
+    while true do
+        wait(math.random(100, 130))
+        game.ReplicatedStorage.GameData.LatestRoom.Changed:Wait()
+        wait(7)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/eoyoustme/Script/refs/heads/main/G-95.txt"))()
+    end
+end)()
+
+-- Enraged Rush
+coroutine.wrap(function()
+    while true do
+        wait(math.random(20, 40))
+        game.ReplicatedStorage.GameData.LatestRoom.Changed:Wait()
+        wait(7)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Anctarus7105/Entities-Spawns/refs/heads/main/ObfuscatedEntities/EnragedRush.lua"))()
+    end
+end)()
+
+-- Smiler
+coroutine.wrap(function()
+    while true do
+        wait(math.random(20, 80))
+        game.ReplicatedStorage.GameData.LatestRoom.Changed:Wait()
+        wait(4)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Anctarus7105/Entities-Spawns/refs/heads/main/SmilerV2"))()
+    end
+end)()
+
+-- Vhs!Sans
+coroutine.wrap(function()
+    while true do
+        wait(math.random(10, 100))
+        game.ReplicatedStorage.GameData.LatestRoom.Changed:Wait()
+        wait(2)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Anctarus7105/Entities-Spawns/refs/heads/main/Vhs!Sans"))()
+    end
+end)()
+
+-- Matcher
+coroutine.wrap(function()
+    while true do
+        wait(math.random(30, 90))
+        game.ReplicatedStorage.GameData.LatestRoom.Changed:Wait()
+        wait(3)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Anctarus7105/Entities-Spawns/refs/heads/main/Matcher.lua"))()
+    end
+end)()
+
+-- Fear
+coroutine.wrap(function()
+    while true do
+        wait(math.random(50, 150))
+        game.ReplicatedStorage.GameData.LatestRoom.Changed:Wait()
+        wait(1)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Anctarus7105/Entities-Spawns/refs/heads/main/FearV2.lua"))()
+    end
+end)()
+
+[[--
+Hello, this is my new mode
+made by eoyoustme, Anctarus#7105 and johnnkcool
+good luck!
+--]]
